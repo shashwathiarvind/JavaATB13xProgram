17a5c0b8d2d5a5c1fae6d25de28e2d76c475b0fb
shashwathi arvind <shashwathi1906@gmail.com>
2025-06-09 19:05:44 +0530

    New adn Lab001 added

---
+++ src/ex_01_Java_Basic/Lab008_Comments.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab008_Comments {
+    /**
+     * Author - ABC
+     * Course : ATB 13X
+     * This is documentation comment
+     */
+    public static void main(String[] args) {
+        //Method implementation
+    }
+}
