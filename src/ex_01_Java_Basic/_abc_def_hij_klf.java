6ff8bdd3c797a939f4c81dbfb4a18e44b4b7a916
shashwathi arvind <shashwathi1906@gmail.com>
2025-06-09 20:24:02 +0530

    New adn Lab001 added

---
+++ src/ex_01_Java_Basic/_abc_def_hij_klf.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class _abc_def_hij_klf {
+    public static void main(String[] args) {
+        System.out.println("Identifiers can be started with $ and _ and can have long name");
+    }
+}
