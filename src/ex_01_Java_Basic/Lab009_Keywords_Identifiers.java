869936cc057fbdfee1c40325a3e17ae402b4569c
shashwathi arvind <shashwathi1906@gmail.com>
2025-06-09 20:22:24 +0530

    New adn Lab001 added

---
+++ src/ex_01_Java_Basic/Lab009_Keywords_Identifiers.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class Lab009_Keywords_Identifiers {
+    public static void main(String[] args) {
+        System.out.println("Hi,Shashwathi");
+            //keywords are in lowercase, in this program package,public,class,static, void are keywords
+
+    }
+}
