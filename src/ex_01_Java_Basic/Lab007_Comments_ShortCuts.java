e18430c0b593d877b74fba0c2751353c9831c268
shashwathi arvind <shashwathi1906@gmail.com>
2025-06-09 19:00:10 +0530

    New adn Lab001 added

---
+++ src/ex_01_Java_Basic/Lab007_Comments_ShortCuts.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab007_Comments_ShortCuts {
+//    This line wont be executed =ctrl backslash
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
