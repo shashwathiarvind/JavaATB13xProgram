a068d0cc3440478237139f52483e5a4524fd350c
shashwathi arvind <shashwathi1906@gmail.com>
2025-06-10 18:01:19 +0530

    New adn Lab001 added

---
+++ src/ex_02_Java_Basics/Lab018_Variables.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basics;
+
+public class Lab018_Variables {
+    public static void main(String[] args) {
+        //different ways of declaring integers
+        int _ = 65;
+        int $ = 65;
+        int abc = 65 ;
+        int this_is_a_long_value = 90;
+
+    }
+}
