97439e7e1349ffcf10b9d6d1bd4b624b1ae5ea31
shashwathi arvind <shashwathi1906@gmail.com>
2025-06-10 17:47:36 +0530

    New adn Lab001 added

---
+++ src/ex_02_Java_Basics/Lab014_Variables.java
@@ -0,0 +1,16 @@
+package ex_02_Java_Basics;
+
+public class Lab014_Variables {
+//    we can have multiple main functions with diffeent parameters
+    public static void main(String[] args) {
+
+    }
+
+    public static void main(int args) {
+
+    }
+
+    public static void main() {
+
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab015_Variables.java
@@ -0,0 +1,16 @@
+package ex_02_Java_Basics;
+
+public class Lab015_Variables {
+    public static void main(String[] args) {
+        //
+         //data_type variable_name = variable_value
+        //
+        byte b = 10;
+        //byte = data_type    - type of container
+        //stores only integer value
+        //-128 to 127
+
+        // b-> variable_name->identifier
+        //10-> literal -> integer value
+    }
+}
