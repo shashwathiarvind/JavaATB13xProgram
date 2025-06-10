bd1ec3f18e91f5382139760d7807ad10d8cbdcbb
shashwathi arvind <shashwathi1906@gmail.com>
2025-06-10 22:18:25 +0530

    New adn Lab001 added

---
+++ src/ex_02_Java_Basics/Lab027_Constants.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basics;
+
+public class Lab027_Constants {
+    public static void main(String[] args) {
+       final int aa = 10;
+        //aa = 11;
+        System.out.println(aa);
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab028_Local_Variable.java
@@ -0,0 +1,13 @@
+package ex_02_Java_Basics;
+
+public class Lab028_Local_Variable {
+    public static void main(String[] args) {
+        int a = 10;
+        byte b = 20;
+        char c = 'A';
+        boolean b1 = true;
+        short s = 10;
+        //everything inside this main braces is local variable
+
+    }
+}
