5f0b51c3804156dcdeeac3caf03749521cac7bc7
shashwathi arvind <shashwathi1906@gmail.com>
2025-06-10 22:50:13 +0530

    New adn Lab001 added

---
+++ src/ex_03_Literals/Lab031_Char_Literals.java
@@ -0,0 +1,16 @@
+package ex_03_Literals;
+
+public class Lab031_Char_Literals {
+    public static void main(String[] args) {
+        char c1 = 'A';
+        char c2 = '@';
+        char c3 = '$';
+        //escape char
+        char new_line = '\n';
+        char tab_line = '\t';
+        char back_space = '\b';
+        System.out.println("hi");
+        System.out.println("hi" +new_line+ "how are you");
+        System.out.println("hi" +tab_line+ "hiiiiiiii");
+    }
+}
