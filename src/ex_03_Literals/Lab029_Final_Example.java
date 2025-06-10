9370d3e9ff74cdb1dcdfbc6693ab69bb0c3d3e0c
shashwathi arvind <shashwathi1906@gmail.com>
2025-06-10 22:30:41 +0530

    New adn Lab001 added

---
+++ src/ex_03_Literals/Lab029_Final_Example.java
@@ -0,0 +1,9 @@
+package ex_03_Literals;
+
+public class Lab029_Final_Example {
+    public static void main(String[] args) {
+        int local ;
+        local = 10;
+        System.out.println(local);
+    }
+}
---
+++ src/ex_03_Literals/Lab030_Literals_p1.java
@@ -0,0 +1,8 @@
+package ex_03_Literals;
+
+public class Lab030_Literals_p1 {
+    public static void main(String[] args) {
+        int age = 65; // here 65 value tat is assigned is literal
+
+    }
+}
