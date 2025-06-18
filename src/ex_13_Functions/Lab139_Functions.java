9e787d6dbde43f2437a2b1d0ee28678df93c219f
shashwathi arvind <shashwathi1906@gmail.com>
2025-06-18 22:05:45 +0530

    New adn Lab001 added

---
+++ src/ex_13_Functions/Lab139_Functions.java
@@ -0,0 +1,11 @@
+package ex_13_Functions;
+
+public class Lab139_Functions {
+    public static void main(String[] args) {
+        //Built-in functions
+        //created by Java guys
+        int result = Math.min(3,4);
+        System.out.println(result);
+
+    }
+}
