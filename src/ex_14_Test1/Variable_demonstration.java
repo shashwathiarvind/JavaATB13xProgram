04950f0cdf8fc2cdc962d2be60e1cd3ef930180a
shashwathi arvind <shashwathi1906@gmail.com>
2025-06-19 19:01:24 +0530

    New adn Lab001 added

---
+++ src/ex_14_Test1/Variable_demonstration.java
@@ -0,0 +1,15 @@
+package ex_14_Test1;
+
+public class Variable_demonstration {
+    public static void main(String[] args) {
+        int age = 10;
+        double d = 4.60;
+        boolean b = true;
+        char grade = 'A';
+        //print all variables
+        System.out.println(age);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(grade);
+    }
+}
