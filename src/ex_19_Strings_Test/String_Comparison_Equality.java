406abc17b3282a7d524b5a759e7bbd5a6a37c442
shashwathi arvind <shashwathi1906@gmail.com>
2025-06-26 10:18:18 +0530

    New adn Lab001 added

---
+++ src/ex_19_Strings_Test/String_Comparison_Equality.java
@@ -0,0 +1,35 @@
+package ex_19_Strings_Test;
+
+public class String_Comparison_Equality {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = new String("Hello"); // Created using new keyword
+        String s4 = "hello";
+        String s5 = "World";
+
+        // Using == operator
+        System.out.println("--- Using == operator ---");
+        System.out.println((s1 == s2)); // true as it is in SCP
+        System.out.println( (s1 == s3)); // false
+
+        //  Using equals() method compares content
+        System.out.println("--- Using equals() method (compares content, case-sensitive) ---");
+        System.out.println( s1.equals(s2)); // true
+        System.out.println( s1.equals(s3)); // true
+        System.out.println( s1.equals(s4)); // false
+        System.out.println( s1.equals(s5)); // false
+
+        //  Using equalsIgnoreCase() method it compares content, case-insensitive
+        System.out.println(" Using equalsIgnoreCase() method (compares content, case-insensitive) ---");
+        System.out.println( s1.equalsIgnoreCase(s2)); // true
+        System.out.println( s1.equalsIgnoreCase(s4)); // true
+
+        //  Using compareTo() method
+        System.out.println("--- Using compareTo() method ---");
+
+        System.out.println( s1.compareTo(s2)); // 0
+        System.out.println( s1.compareTo(s5)); // Negative value (H comes before W)
+        System.out.println( s5.compareTo(s1)); // Positive value (W comes after H)
+    }
+}
