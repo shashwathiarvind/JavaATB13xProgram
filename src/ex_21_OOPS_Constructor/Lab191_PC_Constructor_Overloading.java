fe74591e77cb719e8ce316d9618f85a693805c99
shashwathi arvind <shashwathi1906@gmail.com>
2025-07-02 18:10:16 +0530

    New adn Lab001 added

---
+++ src/ex_21_OOPS_Constructor/Lab191_PC_Constructor_Overloading.java
@@ -0,0 +1,53 @@
+package ex_21_OOPS_Constructor;
+
+public class Lab191_PC_Constructor_Overloading {
+    public static void main(String[] args) {
+        Person1 p1 = new Person1("Shashwathi",97654321,"abc");
+        Person1 p2 = new Person1("Arvind",98989898,"XYZ");
+
+        Person1 p3  = new Person1();
+        p3.name = "Kshamya";
+        System.out.println(p3.name);
+
+        Person1 p4 = new Person1("Rekha",823424242);
+        System.out.println(p4.address);
+
+
+        p1.eat();
+        p2.eat();
+        p3.eat();
+        p4.eat();
+    }
+}
+
+class Person1{
+
+    String name;
+    long phone;
+    String address;
+
+    Person1(){
+
+    }
+
+    Person1(String name_arg,long phone_arg, String address_arg){
+        this.name = name_arg;
+        this.address = address_arg;
+        this.phone = phone_arg;
+
+    }
+
+    Person1(String name_arg,long phone_arg){
+        this.name = name_arg;
+        this.phone = phone_arg;
+
+    }
+
+    void eat(){
+        System.out.println(this.name + " is eating!");
+    }
+
+
+}
+
+
